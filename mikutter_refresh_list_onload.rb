92b8b645c313c0e0027eeb21e190be1808df8541
一稀 <oishikazuki@gmail.com>
2014-06-15 16:32:39 +0900

    リストを更新するボタンの実装

--- mikutter_refresh_list_onload.rb
+++ mikutter_refresh_list_onload.rb
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 
 Plugin.create :mikutter_refresh_list_onload do
-    Plugin[:list].fetch_list_of_service(Service.primary).next{
-        Plugin[:list].timelines.values.each{ |list|
-          Plugin[:list].list_modify_member(list)
-        }
-    }
+	Plugin[:list].fetch_list_of_service(Service.primary).next{
+		Plugin[:list].timelines.values.each{ |list|
+			Plugin[:list].list_modify_member(list)
+		}
+	}
+
+	command(:open_my_profile,
+		name: 'リストを更新する',
+		condition: lambda{ |opt| true },
+		visible: true,
+		icon: File.dirname(__FILE__) + "/list.png",
+		role: :window) do |opt|
+
+		Plugin[:list].fetch_list_of_service(Service.primary).next{
+			Plugin[:list].timelines.values.each{ |list|
+				Plugin[:list].list_modify_member(list)
+			}
+		}
+	end
+
 end
