1595cd002fff10cc87f3822a7e083d1af34d1ffc
一稀 <oishikazuki@gmail.com>
2014-06-15 15:50:50 +0900

    initial commit.

---
+++ mikutter_refresh_list_onload.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+Plugin.create :mikutter_refresh_list_onload do
+    Plugin[:list].fetch_list_of_service(Service.primary).next{
+        Plugin[:list].timelines.values.each{ |list|
+          Plugin[:list].list_modify_member(list)
+        }
+	}
+end
---
+++ spec
@@ -0,0 +1,10 @@
+---
+slug: :mikutter_refresh_list_onload
+depends:
+  mikutter: 0.2.2.1537
+  plugin:
+  - gtk
+version: '0.1.0'
+author: oishikazuki
+name: mikutter_refresh_list_onload
+description: 起動時にリストのタイムラインを更新します
